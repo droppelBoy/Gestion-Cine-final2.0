5facbf21077b39727936c1fa0e595ab1405f304e
Esteban Droppelmann-Pablo Muñoz <droppelestebita@gmail.com>
2026-09-25 23:10:53 -0300

    feat: configurar simulacion de API con MSW y Axios

---
+++ src/mocks/data.js
@@ -0,0 +1,46 @@
+export const peliculasIniciales = [
+  {
+    id: 1,
+    title: "Spider-Man: Un Nuevo Día",
+    genre: "Acción / Superhéroes",
+    price: 4500,
+    duration: "2h 24min",
+    rating: "TE+7",
+    synopsis: "Peter Parker inicia una nueva etapa en su vida intentando equilibrar sus responsabilidades cotidianas con la protección de las calles.",
+    image: "/spiderman.jpg",
+    showtimes: ["14:30", "17:45", "21:00"]
+  },
+  {
+    id: 2,
+    title: "Avengers: Endgame (Encore)",
+    genre: "Acción / Ciencia Ficción",
+    price: 5000,
+    duration: "3h 03min",
+    rating: "TE+7",
+    synopsis: "El reestreno de la batalla definitiva de los Vengadores contra Thanos para revertir el chasquido y restaurar el orden en el universo.",
+    image: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UX1000_.jpg",
+    showtimes: ["15:00", "19:00", "22:30"]
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    genre: "Ciencia Ficción / Aventura",
+    price: 5500,
+    duration: "2h 49min",
+    rating: "TE+7",
+    synopsis: "Un grupo de exploradores viaja a través del espacio en busca de un nuevo hogar para la humanidad.",
+    image: "/interstellar.jpg",
+    showtimes: ["16:00", "20:00", "23:00"]
+  },
+  {
+    id: 4,
+    title: "Pinocho: La Película",
+    genre: "Fantasía / Animación",
+    price: 4000,
+    duration: "1h 54min",
+    rating: "TE",
+    synopsis: "La clásica historia del muñeco de madera creado por Geppetto que cobra vida mágicamente y sueña con convertirse en un niño de verdad.",
+    image: "/pinocho.webp",
+    showtimes: ["13:00", "15:30", "18:00"]
+  }
+];
